5df6a7579f0d16047678f5b85cb2611295b42168
Takuto-Deguchi <femuto01040507@gmail.com>
2022-04-25 15:47:03 +0900

    db-test

--- server-middleware/api/routerIndex.js
+++ server-middleware/api/routerIndex.js
@@ -1,17 +1,19 @@
-import knex from "../database/main"
+// import knex from "../database/main"
 const express = require('express');
 const router = express.Router();
 
 // const axios = require('axios');
 
-router.get('/test', async(req, res, next) => {
-   try {
-       const data=await knex('table_name').select('*')
-       console.log(data)
-       res.status(200).end()
-   } catch (error) {
-       console.log(error)
-   }
+router.get('/test',(req, res, next) => {
+//    try {
+//        const data=await knex('table_name').select('*')
+//        console.log(data)
+//        res.status(200).json({ok:true,body:data})
+//    } catch (error) {
+//        console.log(error)
+//    }
+       res.status(200).json({ok:true,body:{test:"test"}})
+
 })
 
 
